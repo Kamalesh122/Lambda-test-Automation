9ad203708eaedd6a31dd3334b6190ddec6f25a7d
ASPIRESYS/kamalesh.sakthi <kamalesh.sakthi@savvas.com>
2023-10-17 19:42:09 +0530

    Initial commit

---
+++ lib/src/test/java/Lambda/test/Automation/AutomationTask.java
@@ -0,0 +1,108 @@
+package Lambda.test.Automation;
+
+import static org.testng.Assert.assertEquals;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class AutomationTask {
+
+	RemoteWebDriver driver;
+
+	@BeforeMethod
+	public void setup() throws MalformedURLException {
+
+		String userName = "kamalesh.sakthi";
+
+		String accessKey = "TwkiAiPxTjq6mcModff6mNmVRZh6VLrOuCwI1o3bYpkC5o2Ah1";
+
+		String hub = "@hub.lambdatest.com/wd/hub";
+
+		ChromeOptions browserOptions = new ChromeOptions();
+		browserOptions.setPlatformName("Windows 10");
+		browserOptions.setBrowserVersion("86.0");
+		HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+		ltOptions.put("username", userName);
+		ltOptions.put("accessKey", accessKey);
+		ltOptions.put("visual", true);
+		ltOptions.put("video", true);
+		ltOptions.put("build", "Lamdatest-Assignment");
+		ltOptions.put("network", true);
+		ltOptions.put("console", true);
+		ltOptions.put("project", "Untitled");
+		ltOptions.put("selenium_version", "4.0.0");
+		ltOptions.put("w3c", true);
+		browserOptions.setCapability("LT:Options", ltOptions);
+
+		driver = new RemoteWebDriver(new URL("https://" + userName + ":" + accessKey + hub), browserOptions);
+
+	}
+
+	@Test
+	public void test001() throws InterruptedException, MalformedURLException {
+
+		driver.get("https://www.lambdatest.com");
+		String ExpectedUrl = "https://www.lambdatest.com/integrations";
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+		WebElement header = driver.findElement(By.id("header"));
+		wait.until(ExpectedConditions.visibilityOf(header));
+
+		WebElement seeAllIntegraion = driver.findElement(By.cssSelector(".clearfix a[href*='integration']"));
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);",
+				driver.findElement(By.xpath("//*[@class='clearfix'] //ul")));
+
+		// seeAllIntegraion.click();
+		String intLink = seeAllIntegraion.getAttribute("href");
+
+		driver.switchTo().newWindow(WindowType.TAB);
+
+		ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
+
+		System.out.println(windows);
+
+		driver.get(intLink);
+
+		WebElement intHeading = driver.findElement(By.xpath("//h1[text()='LambdaTest Integrations']"));
+		wait.until(ExpectedConditions.visibilityOf(intHeading));
+
+		String ActualUrl = driver.getCurrentUrl();
+
+		// assertEquals(ExpectedUrl,ActualUrl,,);
+
+		assertEquals(ExpectedUrl, ActualUrl, "Verified the current Url is matched with expected Url");
+
+		System.out.println("Closing the current window...");
+		driver.close();
+
+		driver.quit();
+
+	}
+
+	@AfterMethod
+	public void quit() {
+		System.out.println("Closing the current window");
+		driver.quit();
+
+	}
+
+}
